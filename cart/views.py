c98319ab9e5621ec53c32d505bd57a5807625346
SiThuTun22 <sithu1492004@gmail.com>
2024-12-10 20:31:30 +0630

    first commit

---
+++ cart/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render,get_object_or_404
+from .cart import Cart
+from store.models import Product
+from django.http import JsonResponse
+# Create your views here.
+def cart_summary(request):
+  cart = Cart(request)
+  cart_products = cart.get_prods
+  return render(request,"cart_summary.html",{"cart_products":cart_products})
+
+def cart_add(request):
+  # Get The cart
+  cart = Cart(request)
+  # test for Post
+  if request.POST.get('action') == "post":
+    # Get stuff
+    product_id = int(request.POST.get('product_id'))
+    product_qty = int(request.POST.get('product_qty'))
+    # lookup product in DB
+    product = get_object_or_404(Product,id=product_id)
+    # Save to session
+    cart.add(product=product)
+    # GEt cart quantity
+    cart_quantity = cart.__len__()
+
+    #return response
+    # response = JsonResponse({'Product Name: ':product.name})
+    response = JsonResponse({'qty':cart_quantity})
+    return response
+def cart_delete(request):
+  pass
+
+def cart_update(request):
+  pass
